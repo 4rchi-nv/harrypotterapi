745a87c347114f6180bdb4e144b4e6f4b207ccc5
Archie <agajanov0arslan@gmail.com>
2023-12-08 00:37:13 +0500

    fix: refactoring2

--- src/components/movies/movies.jsx
+++ src/components/movies/movies.jsx
@@ -1,31 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import './movies.css'
+import { useMovies } from "../../hooks/useMovies";
 
-const MoviesList = () => {
-    const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                const response = await fetch('https://api.potterdb.com/v1/movies');
-                if (!response.ok) {
-                    throw new Error('Failed to fetch');
-                }
-                const dataset = await response.json();
-                const { data } = dataset;
-                console.log(data);
-                setMovies(data);
-            } catch (error) {
-                console.error(error.message);
-            } finally {
-                setLoading(false); // Set loading to false regardless of success or failure
-            }
-        }
-        fetchMovies();
-    }, []);
-
-    return (
+export const MoviesList = () => {
+    const { movies, loading } = useMovies();
+        return (
         <div className="moviesContainer">
             <h1>Harry Potter Movies</h1>
             {loading ? (
@@ -58,5 +37,3 @@ const MoviesList = () => {
         </div>
     );
 };
-
-export default MoviesList;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './components/styles.css'
-import MoviesList from './components/movies/movies';
+import { MoviesList } from './components/movies/movies';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
